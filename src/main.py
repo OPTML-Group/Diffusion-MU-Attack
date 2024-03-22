34044b1dfda621bb0065484655497b89198020c9
Xin <xin.chen@intel.com>
2024-03-21 17:39:19 -0700

    [Update] add object and style into main

--- src/main.py
+++ src/main.py
@@ -21,9 +21,21 @@ def run_attack(udiff_input : UdiffInput):
             config_file = "/home/jiqingfe/Diffusion-MU-Attack/configs/nudity/text_grad_esd_nudity_classifier.json"
             results_dir = "/home/jiqingfe/Diffusion-MU-Attack/files/results/text_grad_esd_nudity_classifier/attack_idx_7/log.json"
         else:
-            raise NotImplementedError("Not implemented yet")
+            raise NotImplementedError("Nudity Attack Method Not implemented yet")
+    elif udiff_input.concept == "object":
+        if udiff_input.attacker == "text_grad":
+            config_file = "/home/jiqingfe/Diffusion-MU-Attack/configs/object/text_grad_esd_church_classifier.json"
+            results_dir = "/home/jiqingfe/Diffusion-MU-Attack/files/results/text_grad_esd_church_classifier/attack_idx_7/log.json"
+        else:
+            raise NotImplementedError("Object Attack Method Not implemented yet")
+    elif udiff_input.concept == "style":
+        if udiff_input.attacker == "text_grad":
+            config_file = "/home/jiqingfe/Diffusion-MU-Attack/configs/style/text_grad_esd_vangogh_classsifier.json"
+            results_dir = "/home/jiqingfe/Diffusion-MU-Attack/files/results/text_grad_esd_vangogh_classsifier/attack_idx_7/log.json"
+        else:
+            raise NotImplementedError("Style Attack Method Not implemented yet")
     else:
-        raise NotImplementedError("Not implemented yet")
+        raise NotImplementedError("Attack Concept Not implemented yet")
 
     Main(config_file=config_file, model_name_or_path=udiff_input.model_id)
 
